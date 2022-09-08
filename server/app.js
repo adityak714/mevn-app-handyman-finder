ac2c02c37f1e8f40314845bf7e659d6c989ac856
Aditya Khadkikar <guskhadad@student.gu.se>
2022-09-08 18:10:56 +0200

    feat: make schema for reviews
    + have handymen store collection of reviews
    + test post/get handlers in app.js
    NOTE: need to be moved to controllers that handle this

--- server/app.js
+++ server/app.js
@@ -1,76 +1,98 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var path = require('path');
-var cors = require('cors');
-var history = require('connect-history-api-fallback');
+var express = require("express");
+var mongoose = require("mongoose");
+var morgan = require("morgan");
+var path = require("path");
+var cors = require("cors");
+var history = require("connect-history-api-fallback");
+const reviews = require("./src/Infrastructure/models/ReviewSchema");
 
 // Create Express app
 var app = express();
 
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb+srv://oscareinagus:3tXWisPEbcijWIB4@cluster0.ynhyrmr.mongodb.net/test';
+var mongoURI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://oscareinagus:3tXWisPEbcijWIB4@cluster0.ynhyrmr.mongodb.net/test";
 var port = process.env.PORT || 3000;
 
-app.listen(port, function(err) {
-    if (err) throw err;
-    console.log(`Express server listening on port ${port}, in ${env} mode`);
-    console.log(`Backend: http://localhost:${port}/api/`);
-    console.log(`Frontend (production): http://localhost:${port}/`);
+app.listen(port, function (err) {
+  if (err) throw err;
+  console.log(`Express server listening on port ${port}, in ${env} mode`);
+  console.log(`Backend: http://localhost:${port}/api/`);
+  console.log(`Frontend (production): http://localhost:${port}/`);
 });
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+mongoose.connect(
+  mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err) {
     if (err) {
-        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-        console.error(err.stack);
-        process.exit(1);
+      console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+      console.error(err.stack);
+      process.exit(1);
     }
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
-});
+  }
+);
 
 // Parse requests of content-type 'application/json'
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // HTTP request logger
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Enable cross-origin resource sharing for frontend must be registered before api
-app.options('*', cors());
+app.options("*", cors());
 app.use(cors());
 
 // Import routes
-app.get('/handymen', function(req, res) {
-    res.sendStatus(200);
+app.post("/api/reviews", function (req, res) {
+  let new_review = new Review({
+    reviewer: req.body.reviewer,
+    datetime: req.body.datetime,
+    rating: req.body.rating,
+    comment: req.body.comment,
+  });
+  new_review.save(function (err, new_review) {
+    if (err) { res.send(err); }
+    res.status(201).json(new_review);
+  });
+});
+app.get("/api/reviews", function (req, res) {
+    reviews.find((err, reviews) => {
+        if (err) { res.send(err);}
+        res.status(200).json({"reviews": reviews});
+    });
 });
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
-app.use('/api/*', function (req, res) {
-    res.status(404).json({ 'message': 'Not Found' });
+app.use("/api/*", function (req, res) {
+  res.status(404).json({ message: "Not Found" });
 });
 
 // Configuration for serving frontend in production mode
 // Support Vuejs HTML 5 history mode
 app.use(history());
 // Serve static assets
-var root = path.normalize(__dirname + '/..');
-var client = path.join(root, 'client', 'dist');
+var root = path.normalize(__dirname + "/..");
+var client = path.join(root, "client", "dist");
 app.use(express.static(client));
 
 // Error handler (i.e., when exception is thrown) must be registered last
-var env = app.get('env');
+var env = app.get("env");
 // eslint-disable-next-line no-unused-vars
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    var err_res = {
-        'message': err.message,
-        'error': {}
-    };
-    if (env === 'development') {
-        // Return sensitive stack trace only in dev mode
-        err_res['error'] = err.stack;
-    }
-    res.status(err.status || 500);
-    res.json(err_res);
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  var err_res = {
+    message: err.message,
+    error: {},
+  };
+  if (env === "development") {
+    // Return sensitive stack trace only in dev mode
+    err_res["error"] = err.stack;
+  }
+  res.status(err.status || 500);
+  res.json(err_res);
 });
 
 module.exports = app;
--- server/src/Infrastructure/models/HandyManSchema.js
+++ server/src/Infrastructure/models/HandyManSchema.js
@@ -1,15 +1,16 @@
 var mongoose = require('mongoose');
+var Review = require('./ReviewSchema');
 var Schema = mongoose.Schema;
 
 var handymenSchema = new Schema ({
-    id: { type: String},
-    firstName: { type: String},
-    lastName: { type: String},
-    email: { type: String},
-    phoneNumber : {type: Number},
-    area : {type: String},
-    profession: {type:String}
-
-
+    id: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String },
+    phoneNumber : { type: Number },
+    area : { type: String },
+    profession: { type: String },
+    reviews: { type: [Review] }
 });
+
 module.exports = mongoose.model('handymen', handymenSchema);
