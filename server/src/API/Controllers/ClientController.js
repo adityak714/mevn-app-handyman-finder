2437c7d5a22eafbea29b7a92b6e25caae08d5877
Aditya Khadkikar <guskhadad@student.gu.se>
2022-09-14 01:00:09 +0200

    feat: made PUT and PATCH handlers for client #6 #15
    + The original plan was that PUT handler was only to be used to update visible client
      details like name, email etc, but PATCH was to only
      handle password change functionality that could be incorporated later
      in the app as a use case, as it only changes that field and is
      not idempotent.  Would this be fine or we should allow PATCH
      to still allow updates for any field of the client?

--- server/src/API/Controllers/ClientController.js
+++ server/src/API/Controllers/ClientController.js
@@ -14,23 +14,55 @@ router.post("/api/clients", function (req, res, next) {
 });
 
 //Get all clients
-router.get('/api/clients', function (req, res, next) {
-  Client.find({}, function(err, clients) {
-    if (err) {res.send(err);}
-    res.status(200).json({"clients": clients});
-  })
+router.get("/api/clients", function (req, res, next) {
+  Client.find({}, function (err, clients) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200).json({ clients: clients });
+  });
 });
 
 //Get a specific client
-router.get('/api/clients/:id', function (req, res, next) {
+router.get("/api/clients/:id", function (req, res, next) {
   Client.findById(req.params.id)
     .then((clientFound) => {
       if (clientFound) {
         res.status(200).json(clientFound);
-      }})
+      }
+    })
     .catch((err) => {
       res.send(err);
     });
+});
+
+//Update client profile details
+router.put("/api/clients/:id", function (req, res) {
+  Client.findByIdAndUpdate(req.params.id, req.body)
+    .then((updatedClient) => {
+      if (updatedClient) {
+        res.status(200).json(updatedClient);
+      }
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+});
+
+//Update client password via PATCH
+router.patch("/api/clients/:id", function (req, res) {
+  let updatedClient = Client.findById(req.params.id);
+
+  updatedClient.then((client) => {
+    if (req.body.password) {
+      client.password = req.body.password;
+    }
+    client.save();
+    res.status(200).json('Updated password successfully!');
+  })
+  .catch((clientNotFound) => {
+    res.send(clientNotFound);
+  });
 });
 
 module.exports = router;
