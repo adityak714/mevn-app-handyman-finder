644695419835c45859a8953d94f4c8f01ef7c199
scarr <scarr@student.chalmers.se>
2022-09-13 21:08:13 +0000

    Revert "Merge branch 'feature/#5' into 'master'"
    This reverts merge request !8

--- server/src/Domain/Entities/Client.js
+++
@@ -1,8 +0,0 @@
-import {User} from './User.js';
-
-class Client extends User {
-    constructor(id, firstName, lastName, email, phoneNumber, address){
-        super(id, firstName, lastName, email, phoneNumber, address);
-    }
-}
-
