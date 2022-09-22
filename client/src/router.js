f486dbc34a155a6f43469f23712a54f8cf0cc961
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-09-22 19:07:38 +0200

    Initial Commit

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import LogIn from './views/LogInUser.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogIn
     }
   ]
 })
