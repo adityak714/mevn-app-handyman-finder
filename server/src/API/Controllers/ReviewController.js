9d2c16ce8e48e9d3d5ef0661b1afb4913ec3b1c9
scarr <scarr@student.chalmers.se>
2022-10-11 14:06:14 +0000

    Revert "Merge branch 'revert-cf14b17c' into 'master'"
    This reverts merge request !41

--- server/src/API/Controllers/ReviewController.js
+++ server/src/API/Controllers/ReviewController.js
@@ -12,10 +12,9 @@ router.post("/api/reviews", function (req, res) {
     }
     HandyMan.findById(new_review.to, (err, handyman) => {
       handyman.reviews.push(new_review._id);
-      handyman.save()
-      .then(() => {
+      handyman.save().then(() => {
         return res.status(201).json(new_review);
-      })
+      });
     });
   });
 });
@@ -29,5 +28,76 @@ router.get("/api/reviews", function (req, res) {
     return res.status(200).json({ reviews: reviews });
   });
 });
+
+//Getting a specific review
+router.get("/api/reviews/:id", function (req, res, next) {
+  Review.findById(req.params.id)
+    .then((review) => {
+      return res.status(200).json(review);
+    })
+    .catch((err) => next(err));
+});
+
+//Update a review by id
+router.put("/api/reviews/:id", function (req, res) {
+  Review.findById(req.params.id, (err, review) => {
+    if (err) return res.status(500).send(err);
+    if (!review) {
+      return res.status(404).send('Review could not be found.');
+    }
+  }).then((review) => {
+    review.rating = req.body.rating;
+    review.comment = req.body.comment;
+    review.save(() => {
+      return res.status(200).json(review);
+    });
+  }).catch((err) => {
+    return res.status(500).send(err);
+  })
+});
+
+//Update a review by id
+router.patch("/api/reviews/:id", function (req, res) {
+  Review.findById(req.params.id, (err, review) => {
+    if (err) return res.status(500).send(err);
+    if (!review) {
+      return res.status(404).send('Review could not be found.');
+    }
+  }).then((review) => {
+    review.rating = req.body.rating || review.rating;
+    review.comment = req.body.comment || review.comment;
+    review.save(() => {
+      return res.status(200).json(review);
+    });
+  }).catch((err) => {
+    return res.status(500).send(err);
+  })
+});
+
+//Delete a review by id
+router.delete('/api/reviews/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const review = await Review.findByIdAndDelete(_id);
+    if (!review) return res.status(200).json({
+      "error": "Review does not exist and could not be deleted."
+    });
+   return res.status(204).send(review);
+  } catch (e) {
+   return res.sendStatus(400).send(err);
+  }
+})
+
+//Delete all reviews 
+router.delete('/api/reviews', function(req, res) {
+  Review.deleteMany({}, function(err, reviews) {
+      if (err) {
+        return res.send(err);
+      } if (reviews == null) {
+        return res.status(200).send("No reviews found.");
+      }
+      return res.status(204).send("All review deleted.");
+  })
+})
 
 module.exports = router;
