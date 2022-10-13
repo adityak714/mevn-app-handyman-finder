13ac4e1a37654ab2eb23e9b38310b9145ad62943
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-10-13 21:54:14 +0200

    feat: chance style hanyman request

--- client/src/components/Request.vue
+++ client/src/components/Request.vue
@@ -1,34 +1,23 @@
 <template>
   <div>
-    <b-card no-body class="overflow-hidden">
-      <b-col cols="12" class = "card">
-        <b-row no-gutters class = "card-container">
-
-          <b-row class="first-row">
-            <b-col cols="6" class="date-request">
-              <div class = "handyman-name">
-                <strong>Date: {{request.date}}</strong>
-              </div>
-            </b-col>
-            <b-col cols="6" class="job-request">
-              <div class = "handyman-name">
-                <strong>Job: {{request.job}}</strong>
-              </div>
+    <b-card no-body class="overflow-hidden card">
+      <b-row no-gutters class = "card-container">
+          <b-row class = "card-content">
+            <b-col cols = "12" class="first-row card-row">
+              <strong>Date: oscar</strong>
             </b-col>
           </b-row>
-          <b-row class="second-row">
-            <b-col cols="12" class="description-request">
-              <div class = "handyman-name">
-                <strong>Description: {{request.description}}</strong>
-              </div>
+          <b-row>
+            <b-col cols = "12" class="first-row card-row">
+              <strong>Description: oscar}</strong>
             </b-col>
           </b-row>
-          <b-row class="third-row">
-            <b-col cols="12" class="description-request">
+          <b-row>
+            <b-col cols = "12" class="first-row card-row">
+              <strong>Status: oscar</strong>
             </b-col>
           </b-row>
-        </b-row>
-      </b-col>
+      </b-row>
     </b-card>
   </div>
   <!----
@@ -65,35 +54,6 @@ export default {
 }
 </script>
 <style scoped>
-.left-card {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-}
-.card-container {
-  width: 100%;
-}
-.right-card {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-}
-.request-information {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  font-family: "Open Sans", sans-serif;
-}
-.card-image {
-  height: 80px;
-  width: 80px;
-  border-radius: 30px;
-}
-.card-content {
-    height: 100%;
-}
-
 .card {
   min-height: 100px;
   height: 100%;
@@ -105,5 +65,67 @@ export default {
   display: flex;
   align-items: center;
   flex-direction: row;
+}
+
+.handyman-information {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.card-content {
+  width: 100%;
+}
+
+.modal-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+
+.card-row {
+  max-height: 50px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: left;
+}
+
+.card-image {
+  height: 70px;
+  width: 70px;
+  margin-left: 10px;
+}
+
+.left-card {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+}
+.title-container {
+  display: flex;
+  justify-content: flex-start;
+  align-content: center;
+}
+
+.right-card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+.title{
+  font-size: 30px;
+}
+#job {
+  width: 100%
+}
+
+.card-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
